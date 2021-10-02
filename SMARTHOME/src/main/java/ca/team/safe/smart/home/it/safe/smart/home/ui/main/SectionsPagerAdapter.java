1724d0a305f2135702c6e8a49b1e45c60d6bc76e
JacobStephens0088 <64173148+jacobstephens4@users.noreply.github.com>
2021-10-02 17:44:08 -0400

    Added LoginFragment class and StatusFragment class. Also added these fragments to tabs.

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/SectionsPagerAdapter.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/SectionsPagerAdapter.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import ca.team.safe.smart.home.it.safe.smart.home.LoginFragment;
 import ca.team.safe.smart.home.it.safe.smart.home.R;
+import ca.team.safe.smart.home.it.safe.smart.home.StatusFragment;
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -27,9 +29,16 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch(position){
+            case 0:
+                fragment = new LoginFragment();
+                break;
+            case 1:
+                fragment = new StatusFragment();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
