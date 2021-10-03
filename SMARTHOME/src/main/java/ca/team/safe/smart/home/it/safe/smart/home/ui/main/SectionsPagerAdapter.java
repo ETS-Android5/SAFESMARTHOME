09a73c074da9998d75bbf4a7cabbed53e44362f1
VeeraGudla8714 <70934190+Veera8714@users.noreply.github.com>
2021-10-02 21:04:05 -0400

    Added HumidityFragment.java and Humidity/Fire tab.

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/SectionsPagerAdapter.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/SectionsPagerAdapter.java
@@ -21,7 +21,8 @@ import ca.team.safe.smart.home.it.safe.smart.home.StatusFragment;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3,
+            R.string.tab_text_4, R.string.tab_text_5,};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
