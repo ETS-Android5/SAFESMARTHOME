dff960c105da0c7a2895758e0b656c025ed87687
VeeraGudla8714 <70934190+Veera8714@users.noreply.github.com>
2021-10-06 15:43:15 -0400

    Added splash page.

---
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/Splash.java
@@ -0,0 +1,32 @@
+package ca.team.safe.smart.home.it.safe.smart.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
