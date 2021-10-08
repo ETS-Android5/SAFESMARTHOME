452706cbcd0f91331fed3622b562fc0748c685aa
PatrickLoboda9086 <n01309086@humber.mail.com>
2021-10-08 00:35:52 -0400

    Added and commented out code in ControlFragment due to app crashing. Added new TextView and Switch in fragment_control.xml

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ControlFragment.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ControlFragment.java
@@ -1,18 +1,20 @@
-//Veera Gudla N01218714
-//Jerin Joy N01276691
-//Jacob Stephens N01100888
-//Patrick Loboda N01309086
-
 package ca.team.safe.smart.home.it.safe.smart.home;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class ControlFragment extends Fragment {
+
+    ImageView imageView;
+
+    int stage;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,6 +60,73 @@ public class ControlFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        SharedPreferences preferences = getActivity().getSharedPreferences("PREFS", 0);
+        stage = preferences.getInt("switch_stage", 1);
+
+        imageView = (ImageView) getActivity().findViewById(R.id.controls_tempIV);
+//        setSwitchImage(stage);
+//
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switchNumber();
+//            }
+//        });
         return inflater.inflate(R.layout.fragment_control, container, false);
     }
+
+//    public void switchNumber() {
+//        switch(stage) {
+//            case 1:
+//                stage = 2;
+//                setSwitchImage(stage);
+//                doStuff(stage);
+//                break;
+//            case 2:
+//                stage = 3;
+//                setSwitchImage(stage);
+//                doStuff(stage);
+//                break;
+//            case 3:
+//                stage = 1;
+//                setSwitchImage(stage);
+//                doStuff(stage);
+//                break;
+//        }
+//
+//        //saves state
+//        SharedPreferences preferences = requireContext().getSharedPreferences("PREFS", 0);
+//        SharedPreferences.Editor editor = preferences.edit();
+//        editor.putInt("switch_stage", stage);
+//        editor.apply();
+//    }
+//
+//    //Sets Image of the switch
+//    private void setSwitchImage(int current) {
+//        switch (current) {
+//            case 1:
+//                imageView.setImageResource(R.drawable.ic_baseline_power_settings_new_24);
+//                break;
+//            case 2:
+//                imageView.setImageResource(R.drawable.ic_baseline_ac_unit_24);
+//                break;
+//            case 3:
+//                imageView.setImageResource(R.drawable.ic_baseline_wb_sunny_24);
+//                break;
+//        }
+//    }
+//
+//    public void doStuff(int current) {
+//        switch (current) {
+//            case 1:
+//                Toast.makeText(getActivity(), "Temperature is " + current, Toast.LENGTH_SHORT).show();
+//                break;
+//            case 2:
+//                Toast.makeText(getActivity(), "Temperature is " + current, Toast.LENGTH_SHORT).show();
+//                break;
+//            case 3:
+//                Toast.makeText(getActivity(), "Temperature is " + current, Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//    }
 }
