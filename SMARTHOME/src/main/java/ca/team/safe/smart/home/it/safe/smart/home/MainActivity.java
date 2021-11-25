377ebe23750de4dc7da072804e63d83708131e15
shrig <vrkgudla@gmail.com>
2021-11-25 13:25:34 -0500

    Added separate login page away from the tabs.

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/MainActivity.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/MainActivity.java
@@ -64,10 +64,12 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        String reg=getIntent().getStringExtra("register");
-        if (reg.equals("reg")){
-            viewPager.setCurrentItem(6);
-        }
+        try {
+            String reg = getIntent().getStringExtra("register");
+            if (reg.equals("reg")) {
+                viewPager.setCurrentItem(6);
+            }
+        }catch (Exception e){}
 
 
         // Write a message to the database
