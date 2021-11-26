1c3c7eb343afea181ea0838be101976a64ae3346
shrig <vrkgudla@gmail.com>
2021-11-26 10:57:58 -0500

    Added java code on login fragment to make password at least 8 in length and tested.

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/LoginSep.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/LoginSep.java
@@ -20,6 +20,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginSep extends AppCompatActivity {
     String email, pass, secureID;
     View view;
@@ -69,7 +72,12 @@ public class LoginSep extends AppCompatActivity {
                     Snackbar.make(view, "Please enter correct gmail address", Snackbar.LENGTH_SHORT).show();
                 } else if (pass.equals("")) {
                     Snackbar.make(view, "Please enter password", Snackbar.LENGTH_SHORT).show();
-                } else if (secureID.length() != 9) {
+                } else if(pass.length()<8 &&!isValidPassword(pass)){
+                    Snackbar.make(view,  "Password should be at least 8 in length, alphabetic and numeric, 1 " +
+                                    "special character(except for +,~), at 1 uppercase letter, and at least 1 digit",
+                            Snackbar.LENGTH_SHORT).show();
+                }
+                else if (secureID.length() != 9) {
                     Snackbar.make(view, "Please enter 9 digit Secure ID", Snackbar.LENGTH_SHORT).show();
                 } else {
                     Intent i = new Intent(LoginSep.this, MainActivity.class);
@@ -77,5 +85,16 @@ public class LoginSep extends AppCompatActivity {
                 }
             }
         });
+    }
+    public static boolean isValidPassword(final String password) {
+
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
     }
 }
