85f762671ac99eb4b5a72e37d22ee9013cc3d962
shrig <vrkgudla@gmail.com>
2022-02-22 02:47:36 -0500

    Added a RegisterSep activity.

---
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/HelpPage.java
@@ -0,0 +1,18 @@
+package ca.team.safe.smart.home.it.safe.smart.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import ca.team.safe.smart.home.it.safe.smart.home.R;
+
+
+public class HelpPage extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helppage);
+
+    }
+}
+
+
