73f9f2e8921a26b22a3524c60dfe52c438a57579
shrig <vrkgudla@gmail.com>
2022-03-05 00:44:49 -0500

    Created Pop-up display messages on click of 2 buttons in the help page.

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/HelpActivity.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/HelpActivity.java
@@ -1,14 +1,74 @@
 package ca.team.safe.smart.home.it.safe.smart.home;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class HelpActivity extends AppCompatActivity {
-
+Button btnUp,btnReportProblem;
+RadioGroup radioButtonGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        btnUp=findViewById(R.id.btnUp);
+        btnReportProblem=findViewById(R.id.btnReportProblem);
+        radioButtonGroup=findViewById(R.id.radioGroup);
+        btnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
+                View radioButton = radioButtonGroup.findViewById(radioButtonID);
+                int idx = radioButtonGroup.indexOfChild(radioButton);
+                RadioButton r = (RadioButton) radioButtonGroup.getChildAt(idx);
+                String selectedtext = r.getText().toString();
+                showDialog("","Internet connection : Please restart your router and reconnect the cable into the module. wait 30 sec before trying it \n\n" +
+                        "Hardware: Please unplug the power supply and wait 1 min before reconnecting it again.\n\n" +
+                        "False Data: Please contact customer support \n\n" +
+                        "setup hardware: Please connect the power supply first, second connect to the internet, third setup your account.");
+
+//                if (selectedtext.contains("Internet")){
+//                    showDialog("","Please restart your router and reconnect the cable into the module. wait 30 sec before trying it");
+//                }else if (selectedtext.contains("Hardware")){
+//                    showDialog("","Please unplug the power supply and wait 1 min before reconnecting it again.");
+//                }else if (selectedtext.contains("Data")){
+//                    showDialog("","Please contact customer support");
+//                }else {
+//                    showDialog(""," Please connect the power supply first, second connect to the internet, third setup your account.");
+//                }
+            }
+        });
+        btnReportProblem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog("","Thank you for reporting");
+            }
+        });
+    }
+
+    void showDialog(String title,String message){
+        AlertDialog alertDialog = new AlertDialog.Builder(HelpActivity.this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+
+        final Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+        LinearLayout.LayoutParams positiveButtonLL = (LinearLayout.LayoutParams) positiveButton.getLayoutParams();
+        positiveButtonLL.gravity = Gravity.CENTER;
+        positiveButton.setLayoutParams(positiveButtonLL);
     }
 }
