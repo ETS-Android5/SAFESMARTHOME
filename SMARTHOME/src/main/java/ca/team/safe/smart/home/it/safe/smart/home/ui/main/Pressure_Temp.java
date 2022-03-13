4a03121aae3e6d0e19a32c3bd9d3e62047e80573
shrig <vrkgudla@gmail.com>
2022-03-13 04:12:38 -0400

    Created Pressure_Temp page to display pressure and temperature.

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/Pressure_Temp.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/Pressure_Temp.java
@@ -31,7 +31,7 @@ public class Pressure_Temp  extends AppCompatActivity {
         setContentView(R.layout.activity_pressure_temp);
         Dis = findViewById(R.id.Dis);
         FirebaseApp.initializeApp(this);
-getCustomerAddress();
+        getCustomerAddress();
     }
 
 
