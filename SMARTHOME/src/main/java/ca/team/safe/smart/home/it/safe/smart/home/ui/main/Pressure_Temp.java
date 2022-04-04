6f51c2220428b019237a197c3a626ba6c192c42a
vrkgu <vrkgudla@gmail.com>
2022-04-03 20:30:27 -0400

    Changed each page to uniform colour.

--- SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/Pressure_Temp.java
+++ SMARTHOME/src/main/java/ca/team/safe/smart/home/it/safe/smart/home/ui/main/Pressure_Temp.java
@@ -25,21 +25,24 @@ import ca.team.safe.smart.home.it.safe.smart.home.R;
 public class Pressure_Temp  extends AppCompatActivity {
     View view;
     TextView Dis;
+    TextView Per;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pressure_temp);
         Dis = findViewById(R.id.Dis);
+        Per = findViewById(R.id.txtPressure);
         FirebaseApp.initializeApp(this);
-        getCustomerAddress();
+        getTemp();
+        getPress();
     }
 
 
 
-    public void getCustomerAddress() {
+    public void getTemp() {
 
-        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("BME280");
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Temp");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -69,6 +72,54 @@ public class Pressure_Temp  extends AppCompatActivity {
                          public void onFinish() {
 
                          }
+
+                        }.start();
+
+                    }
+
+                }catch (Exception e){}
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+
+    public void getPress() {
+
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Pressure");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //getting values on hashmap from firebase in random order
+                List<String> d1 = new ArrayList<>();
+                String value;
+                //getting values from firebase , setting on array list
+                for(DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    //get value one by one in "value" variable
+                    //  value=String.valueOf(postSnapshot.getValue());
+                    value="Temp= "+String.valueOf(postSnapshot.getValue());
+                   // value=value+" , Pressure = "+postSnapshot.getValue();
+
+                    if (!value.contains("null"))
+                        d1.add(value);
+                }
+                try {
+                    if (d1.size()>0)
+                    {
+                        final int[] i = {0};
+                        new CountDownTimer(1000*d1.size(), 1000) {
+                            public void onTick(long millisUntilFinished) {
+                                Per.setText(" "+d1.get(i[0]));
+                                i[0]++;
+                            }
+
+                            public void onFinish() {
+
+                            }
 
                         }.start();
 
